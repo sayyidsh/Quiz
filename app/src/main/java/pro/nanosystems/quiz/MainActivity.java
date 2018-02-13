e73d68a1e525e005b136a63a9f23afceef364c78
Sayyid Shaban <sayyid.ls@gail.com>
2018-02-13 13:55:04 +0200

    Initial commit

---
+++ app/src/main/java/pro/nanosystems/quiz/MainActivity.java
@@ -0,0 +1,24 @@
+package pro.nanosystems.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void correct(View view) {
+
+        Toast.makeText(this, R.string.correct, Toast.LENGTH_SHORT).show();
+    }
+
+    public void inCorrect(View view) {
+        Toast.makeText(this, R.string.incorrectAnswer, Toast.LENGTH_SHORT).show();
+    }
+}
